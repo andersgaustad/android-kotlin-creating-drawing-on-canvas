e64098a27d7842e8305f8246d528e4abb549c039
andersgaustad <anders.gaustad@gmail.com>
2020-06-27 23:29:44 +0200

    Now possible to draw in app!

--- app/src/main/java/com/example/android/minipaint/canvas/MyCanvasView.kt
+++ app/src/main/java/com/example/android/minipaint/canvas/MyCanvasView.kt
@@ -3,20 +3,53 @@ package com.example.android.minipaint.canvas
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Path
+import android.view.MotionEvent
 import android.view.View
+import android.view.ViewConfiguration
 import androidx.core.content.res.ResourcesCompat
 import com.example.android.minipaint.R
+import kotlin.math.abs
+
+private const val STROKE_WIDTH = 12f
+
+private var path = Path()
 
 class MyCanvasView(context: Context) : View(context) {
     private lateinit var extraCanvas: Canvas
     private lateinit var extraBitmap: Bitmap
+
+    private var motionTouchEventX = 0f
+    private var motionTouchEventY = 0f
+
+    private var currentX = 0f
+    private var currentY = 0f
 
     private val backgroundColor = ResourcesCompat.getColor(
         resources,
         R.color.colorBackground,
         null)
 
+    private val drawColor = ResourcesCompat.getColor(resources, R.color.colorPaint, null)
 
+    private val touchTolerance = ViewConfiguration.get(context).scaledTouchSlop
+
+    // Set up the paint with which to draw.
+    private val paint = Paint().apply {
+        color = drawColor
+        // Smooths out edges of what is drawn without affecting shape.
+        isAntiAlias = true
+        // Dithering affects how colors with higher-precision than the device are down-sampled.
+        isDither = true
+        style = Paint.Style.STROKE // default: FILL
+        strokeJoin = Paint.Join.ROUND // default: MITER
+        strokeCap = Paint.Cap.ROUND // default: BUTT
+        strokeWidth = STROKE_WIDTH // default: Hairline-width (really thin)
+    }
+
+
+    // Overrides
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
 
@@ -33,6 +66,53 @@ class MyCanvasView(context: Context) : View(context) {
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
         canvas?.drawBitmap(extraBitmap, 0f, 0f, null)
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        return if (event != null) {
+            motionTouchEventX = event.x
+            motionTouchEventY = event.y
+
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> touchStart()
+                MotionEvent.ACTION_MOVE -> touchMove()
+                MotionEvent.ACTION_UP -> touchUp()
+            }
+
+            true
+        } else {
+             super.onTouchEvent(event)
+
+        }
+
+
+    }
+
+    // Helpers
+    private fun touchStart() {
+        path.reset()
+        path.moveTo(motionTouchEventX, motionTouchEventY)
+        currentX = motionTouchEventX
+        currentY = motionTouchEventY
+    }
+
+    private fun touchMove() {
+        val dx = abs(motionTouchEventX - currentX)
+        val dy = abs(motionTouchEventY - currentY)
+        if (dx >= touchTolerance || dy >= touchTolerance) {
+            // QuadTo() adds a quadratic bezier from the last point,
+            // approaching control point (x1,y1), and ending at (x2,y2).
+            path.quadTo(currentX, currentY, (motionTouchEventX + currentX) / 2, (motionTouchEventY + currentY) / 2)
+            currentX = motionTouchEventX
+            currentY = motionTouchEventY
+            // Draw the path in the extra bitmap to cache it.
+            extraCanvas.drawPath(path, paint)
+        }
+        invalidate()
+    }
+
+    private fun touchUp() {
+        path.reset()
     }
 
 }
