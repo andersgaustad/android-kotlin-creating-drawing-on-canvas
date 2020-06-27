a0e950c90e4adc3f3dc87594d85659220ccade57
andersgaustad <anders.gaustad@gmail.com>
2020-06-27 21:56:50 +0200

    Created a nice orange background

--- app/src/main/java/com/example/android/minipaint/canvas/MyCanvasView.kt
+++ app/src/main/java/com/example/android/minipaint/canvas/MyCanvasView.kt
@@ -1,8 +1,38 @@
 package com.example.android.minipaint.canvas
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.view.View
+import androidx.core.content.res.ResourcesCompat
+import com.example.android.minipaint.R
 
 class MyCanvasView(context: Context) : View(context) {
+    private lateinit var extraCanvas: Canvas
+    private lateinit var extraBitmap: Bitmap
+
+    private val backgroundColor = ResourcesCompat.getColor(
+        resources,
+        R.color.colorBackground,
+        null)
+
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+
+        // Recycle old bitmap
+        if (::extraBitmap.isInitialized) extraBitmap.recycle()
+
+        extraBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+
+        extraCanvas = Canvas(extraBitmap).apply {
+            drawColor(backgroundColor)
+        }
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        canvas?.drawBitmap(extraBitmap, 0f, 0f, null)
+    }
 
 }
